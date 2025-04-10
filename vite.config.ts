2e5e88226c34015f5c49dab2213109252e1a4ef2
Caleb <flyingmigit8@hotmail.com>
2025-04-10 09:40:39 -0400

    path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/WhatsInMyCloset/",
-  server: { // <--- Add this
-    host: true // '0.0.0.0' or true will listen on all addresses, including LAN and public addresses
-    // Or specify a specific IP:
-    // host: '192.168.1.100' // Replace with your actual network IP
-  }
+  base: "/WhatsInMyCloset/"
 })
